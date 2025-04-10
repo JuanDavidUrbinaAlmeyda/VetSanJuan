dbc69039affb06b8cee8636612e4ac5c6a3fb4be
kingdraw69 <ivaneliecer2004@gmail.com>
2025-04-10 12:18:57 -0500

    parte uno

---
+++ app/Models/veterinario_especialidad.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class veterinario_especialidad extends Model
+{
+    /** @use HasFactory<\Database\Factories\VeterinarioEspecialidadFactory> */
+    use HasFactory;
+}
